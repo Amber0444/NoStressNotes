be978dc9977b59c82f73872e83e254926abb63c1
Alexander Shevelev <alexander.shevelev04@gmail.com>
2026-04-08 22:53:34 +0500

    добавил кастомные цвета для темы приложения и компонентов

---
+++ app/src/main/java/com/rus_euphoria/notes/ui/theme/NoteColors.kt
@@ -0,0 +1,16 @@
+package com.rus_euphoria.notes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object NoteColors {
+    val presets: List<Color> = listOf(
+        Color.White,
+        PastelPink,
+        PastelGreen,
+        PastelBeige,
+        PastelTeal,
+        PastelLavender,
+        PastelYellow,
+        PastelBlue
+    )
+}
